8f652766df7963cca272568ff439f30a66d4a38a
sunkim <kesperinc@msn.com>
2023-03-26 17:48:55 +0900

    Flask and JWT Test

---
+++ flask_test.py
@@ -0,0 +1,29 @@
+import bcrypt
+
+
+from flask import Flask
+app = Flask(__name__)
+
+password = "password"
+encrypted_password = bcrypt.hashpw(password.encode(
+    "utf-8"), bcrypt.gensalt())  # str 객체, bytes로 인코드, salt를 이용하여 암호화
+print(encrypted_password)  # bytes-string
+print(encrypted_password.decode("utf-8"))  # str 객체
+
+"""
+https://justkode.kr/python/flask-restapi-3 : Flask로 REST API 구현하기 참고 
+    pip install bcrypt
+    pip install PyJWT
+    pip install Flask-JWT-Extended
+
+https://edudeveloper.tistory.com/135 : Flask 사용 방법 참고
+"""
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World! '+encrypted_password.decode("utf-8")
+
+
+if __name__ == '__main__':
+    app.run()
